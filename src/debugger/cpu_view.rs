aee93fb3792302bd50446e157799b13afc371e1b
KaOs1 <davellidavide@gmail.com>
2022-03-22 17:25:04 +0100

    Lock free debugger

--- src/debugger/cpu_view.rs
+++ src/debugger/cpu_view.rs
@@ -3,23 +3,20 @@ use std::sync::Arc;
 use crate::debugger::debugger::Debugger;
 
 pub fn show(debugger: Arc<Debugger>, ui: &mut egui::Ui) {
-
-        let state = debugger.cpu_state();
-
+    if let Some(state) = debugger.cpu_state() {
         ui.label("Registers:");
 
         let label = format!(
-                "AF: {:#06X}, BC: {:#06X}, DE: {:#06X}, HL: {:#06X}, PC: {:#06X}, SP: {:#06X}",
-                state.af, state.bc, state.de, state.hl, state.pc, state.sp,
-            );
+            "AF: {:#06X}, BC: {:#06X}, DE: {:#06X}, HL: {:#06X}, PC: {:#06X}, SP: {:#06X}",
+            state.af, state.bc, state.de, state.hl, state.pc, state.sp,
+        );
 
-    ui.label(label);
+        ui.label(label);
 
-    ui.label("Flags:");
-    ui.label(format!(
-                "Z: {}, C: {}, BCD-N: {}, BCD-H: {}",
-                state.zero, state.carry, state.bcd_n, state.bcd_h
-            ));
-
+        ui.label("Flags:");
+        ui.label(format!(
+            "Z: {}, C: {}, BCD-N: {}, BCD-H: {}",
+            state.zero, state.carry, state.bcd_n, state.bcd_h
+        ));
+    }
 }
-
--- src/debugger/debugger.rs
+++ src/debugger/debugger.rs
@@ -3,18 +3,28 @@ use std::sync::{
     Arc, RwLock,
 };
 
-use crate::gbr::cpu::CpuState;
-use crate::gbr::game_boy::GameBoy;
 use crate::gbr::instruction::Instruction;
 use crate::gbr::io_registers::IORegisters;
+use crate::gbr::{bus::Bus, game_boy::GameBoy};
+use crate::gbr::{cpu::CpuState, io_registers};
+
+type AsmState = Vec<(u16, Option<Instruction>)>;
 
 pub struct Debugger {
     pub emu: Arc<RwLock<GameBoy>>,
+    cpu_state: (flume::Sender<CpuState>, flume::Receiver<CpuState>),
+    asm_state: (flume::Sender<AsmState>, flume::Receiver<AsmState>),
+    io_registers_state: (flume::Sender<IORegisters>, flume::Receiver<IORegisters>),
 }
 
 impl Debugger {
     pub fn new(game_boy: Arc<RwLock<GameBoy>>) -> Self {
-        Debugger { emu: game_boy }
+        Debugger {
+            emu: game_boy,
+            cpu_state: flume::unbounded(),
+            asm_state: flume::unbounded(),
+            io_registers_state: flume::unbounded(),
+        }
     }
 
     pub fn step(&self) {
@@ -26,6 +36,11 @@ impl Debugger {
         let (stop_sig, stop_slot) = channel::<()>();
 
         let emu = self.emu.clone();
+
+        let cpu_state_sig = self.cpu_state.0.clone();
+        let asm_state_sig = self.asm_state.0.clone();
+        let io_registers_state_sig = self.io_registers_state.0.clone();
+
         std::thread::spawn(move || {
             let mut emu = emu.write().unwrap();
             loop {
@@ -37,19 +52,38 @@ impl Debugger {
                 if stop_slot.try_recv().is_ok() {
                     break;
                 }
+
+                cpu_state_sig.try_send(emu.cpu().state()).ok();
+                io_registers_state_sig
+                    .try_send(*emu.bus().io_registers())
+                    .ok();
+
+                asm_state_sig
+                    .try_send(Debugger::disassemble(emu.cpu().read_pc(), emu.bus()))
+                    .ok();
             }
         });
 
         stop_sig
     }
 
-    pub fn disassemble(&self) -> Vec<(u16, Option<Instruction>)> {
-        let mut pc = self.emu.read().unwrap().cpu().read_pc();
+    pub fn asm_state(&self) -> Option<AsmState> {
+        self.asm_state.1.try_recv().ok()
+    }
 
-        let mut disassembly = Vec::<(u16, Option<Instruction>)>::new();
+    pub fn cpu_state(&self) -> Option<CpuState> {
+        self.cpu_state.1.try_recv().ok()
+    }
+
+    pub fn io_registers_state(&self) -> Option<IORegisters> {
+        self.io_registers_state.1.try_recv().ok()
+    }
+
+    fn disassemble(mut pc: u16, bus: &Bus) -> AsmState {
+        let mut disassembly = AsmState::new();
 
         for _ in 0..20 {
-            let instruction = match self.emu.read().unwrap().bus().fetch_instruction(pc) {
+            let instruction = match bus.fetch_instruction(pc) {
                 Ok(instr) => instr,
                 Err(e) => {
                     log::error!("dissassemble error: {}", e);
@@ -69,13 +103,5 @@ impl Debugger {
         }
 
         disassembly
-    }
-
-    pub fn cpu_state(&self) -> CpuState {
-        self.emu.read().unwrap().cpu().state()
-    }
-
-    pub fn io_registers(&self) -> IORegisters {
-        *self.emu.read().unwrap().bus().io_registers()
     }
 }
